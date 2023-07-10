c154cad88c02b9d765297caf066ddceb4c61d8a5
unknown <satya-1298>
2023-07-10 08:35:04 +0530

    [Satya]Add : Regex Program

---
+++ Regex_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    //
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex_Display regexDisplay = new Regex_Display();
+            regexDisplay.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Regex_Demo/Regex_Display.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    //A Regex(Regular Expression) is a pattern that used to check wheather a given string 
+    //matches that pattern
+    public  class Regex_Display
+    {
+        static string pattern = "^ce$";
+        // ^ : Indicates the starting of string
+        // $ : Indicates the end of the string.
+        public void Display()
+        {
+            //we have created object of class
+            //we have pass a parameter in given class constructor
+            Regex regex = new Regex(pattern);   
+            if(regex.IsMatch("cen"))
+            {
+                Console.WriteLine("String matches the pattern");
+            }
+            else
+            {
+                Console.WriteLine("string Doesn't matches the pattern");
+            }
+            
+        }
+    }
+}
